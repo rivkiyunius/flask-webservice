e941d888de99bb4233f0ba922b529ea01c617892
“rivki_yunius” <yrivki@binar.co.id>
2019-11-28 16:03:50 +0700

    setting run.py

--- run.py
+++ run.py
@@ -2,8 +2,9 @@ import os
 
 from src.app import create_app
 
-if __name__ == '__main__':
-    env_name = os.getenv('FLASK_ENV')
-    app = create_app(env_name)
+env_name = os.getenv('FLASK_ENV')
+app = create_app(env_name)
 
-    app.run()
+if __name__ == '__main__':
+    port = os.getenv('PORT')
+    app.run(host='0.0.0.0', port=port)
